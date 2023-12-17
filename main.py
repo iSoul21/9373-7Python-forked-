441647ec7cc6026db9e349d49e2b54cf2dc6f106
Kobe <2221513@slu.edu.ph>
2023-12-17 20:22:20 +0000

    add

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 print("Hello 9373 group python!")
 print("hello world")
-print(1+1)
+
+
+def add():
+    result = 1 + 1
+    return result
+
+
+# Call the method and store the result in a variable
+sum_result = add()
+
+# Print the result
+print("1 + 1 =", sum_result)
