de6ff36694bc5922c8a4cebeb6c2394e267ec4dd
Kobe <2221513@slu.edu.ph>
2023-12-17 20:42:21 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
 print("Hello 9373 group python!")
 print("hello world")
-
-
-def add():
-    result = 1 + 1
-    return result
-
-
-# Call the method and store the result in a variable
-sum_result = add()
-
-# Print the result
-print("1 + 1 =", sum_result)
+print("1+1 = ", 2)
